8d485569f2bfc4c23bb2b32d62ce4cbdeee7a760
Francisco Vazquez <franvazgom@gmail.com>
2026-06-01 17:43:33 -0600

    ORM completed

--- code/webpersonal/webpersonal/urls.py
+++ code/webpersonal/webpersonal/urls.py
@@ -17,9 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core import views
+from django.conf.urls.static import static
+from django.conf import settings
+from project import views as views_project
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'), 
+    path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
+    path('project/', views_project.project, name='project'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
