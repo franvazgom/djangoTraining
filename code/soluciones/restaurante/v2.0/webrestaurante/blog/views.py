f6e5fd89e10a025fecf67d1141501959fd9bd3c7
Francisco Vazquez <franvazgom@gmail.com>
2026-06-02 14:24:38 -0600

    Modelos relacionales completed

---
+++ code/soluciones/restaurante/v2.0/webrestaurante/blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Post, Category
+
+
+def blog(request):
+    posts = Post.objects.all()
+    breakpoint()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def category(request, category_id):
+    category = Category.objects.get(id=category_id)
+    return render(request, 'blog/category.html', {'category': category})
