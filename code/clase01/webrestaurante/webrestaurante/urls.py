e8863ceff74cfaf10a317352e3080c5cbad0d750
Francisco Vazquez <franvazgom@gmail.com>
2026-06-10 21:45:59 -0600

    Clase 03

--- code/clase01/webrestaurante/webrestaurante/urls.py
+++ code/clase01/webrestaurante/webrestaurante/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from core import views
+from core.urls import core_urlpatterns
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', include(core_urlpatterns)),
 ]
 
 if settings.DEBUG:
