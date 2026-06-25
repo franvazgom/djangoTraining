ff7ddd492e34e6451e358a216f52a8ebb0ca1455
Francisco Vazquez <franvazgom@gmail.com>
2026-06-25 17:01:34 -0600

    Admin personalizado started

---
+++ code/soluciones/restaurante/v12.0/webrestaurante/pages/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from pages.models import Page
+
+
+class PageAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated', )
+    ordering = ('title', )
+    search_fields = ('title', 'content', )
+    list_display = ('title', 'created', )
+
+
+admin.site.register(Page, PageAdmin)
