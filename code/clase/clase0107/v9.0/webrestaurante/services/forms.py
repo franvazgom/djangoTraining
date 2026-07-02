3609119fd88c91a0f35612ca89fe96683fd23784
Francisco Vazquez <franvazgom@gmail.com>
2026-07-01 21:43:36 -0600

    Clase GET y POST

--- code/clase/clase0107/v9.0/webrestaurante/services/forms.py
+++ code/clase/clase0107/v9.0/webrestaurante/services/forms.py
@@ -1,17 +1,19 @@
-from django.forms import ModelForm, TextInput, Textarea
+from django.forms import ModelForm, TextInput, Textarea, NumberInput
 from services.models import Service
 from django_ckeditor_5.widgets import CKEditor5Widget
+
 
 class ServiceForm(ModelForm):
     class Meta:
         model = Service
-        fields = ['title', 'sub_title', 'content', 'image',]
+        fields = ['title', 'sub_title', 'content', 'image', 'cost',]
         widgets = {
-            'title':TextInput(attrs={'class':'form-control','placeholder':'Título'}),
-            'sub_title':TextInput(attrs={'class':'form-control','placeholder':'Sub título'}),
-            'content':CKEditor5Widget(config_name='extends'),
+            'title': TextInput(attrs={'class': 'form-control', 'placeholder': 'Título'}),
+            'sub_title': TextInput(attrs={'class': 'form-control', 'placeholder': 'Sub título'}),
+            'content': CKEditor5Widget(config_name='extends'),
+            'cost': NumberInput(attrs={'step': '0.01', 'placeholder': '0.00', 'class': 'form-control'}),
         }
-    
+
     def clean(self):
         super(ServiceForm, self).clean()
         title = self.cleaned_data['title']
--- code/clase/clase0107/v9.0/webrestaurante/services/models.py
+++ code/clase/clase0107/v9.0/webrestaurante/services/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django_ckeditor_5.fields import CKEditor5Field
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Service(models.Model):
     title = models.CharField(max_length=100, verbose_name='Título')
     sub_title = models.CharField(max_length=100, verbose_name='Subtítulo')
-    content = CKEditor5Field('Contenido', config_name='extends', blank=True, null=True)
+    content = CKEditor5Field(
+        'Contenido', config_name='extends', blank=True, null=True)
     image = models.ImageField(verbose_name='Imágen', upload_to='services')
+    cost = models.DecimalField(verbose_name='Costo', default=1.0, max_digits=8, decimal_places=2,
+                               validators=[MinValueValidator(1.0), MaxValueValidator(10000.0)])
     created = models.DateTimeField(
         auto_now_add=True, verbose_name='Fecha de creación')
     updated = models.DateTimeField(
